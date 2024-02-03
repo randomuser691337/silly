ed226399c47a58ce3469b764117d0b900c6f672c
randomuser691337 <154252167+randomuser691337@users.noreply.github.com>
2024-02-03 17:57:28 -0600

    fuck

--- assets/apps/rosetoy.js
+++ assets/apps/rosetoy.js
@@ -1,17 +1,4 @@
-// One of my friends annoyed me to add it back, ik that it's immature
-function rosetoy() {
-    const win = `<div id="rose1">
-    <p>Rose Toy app only supports devices with a vibration motor and Chrome on Android.</p>
-    <p>Trying to relax yourself on an iPhone or anything that isnt an Android will not work and may crash your device.</p>
-    <button class="b1" onclick="fesw('rose1', 'rose2');">Start</button></div>
-    <div id="rose2" class="hide">
-      <p>Rose Toy PLUS!</p>
-      <p>Only works on Android w/ Chrome</p>
-      <button class="b1 b2" onclick="rose1();">Pulsing</button>
-      <button class="b1 b2" onclick="vi(20000);">Steady</button>
-    </div>`
-    mkw(win, 'Rose Toy', '240px');
-}
+// One of my friends annoyed me to add it back from WebDesk Canary, ik that it's immature
 function vi(ms) {
     if ("vibrate" in navigator) {
         navigator.vibrate(ms);
@@ -41,5 +28,18 @@ function rose1() {
     vibrate(); // Start the initial vibration
 
     const vibrationInterval = setInterval(vibrate, pattern.reduce((a, b) => a + b, 0));
+}
+function rosetoy() {
+    const win = `<div id="rose1">
+    <p>Rose Toy app only supports devices with a vibration motor and Chrome on Android.</p>
+    <p>Trying to relax yourself on an iPhone or anything that isnt an Android will not work and may crash your device.</p>
+    <button class="b1" onclick="fesw('rose1', 'rose2');">Start</button></div>
+    <div id="rose2" class="hide">
+      <p>Rose Toy PLUS!</p>
+      <p>Only works on Android w/ Chrome</p>
+      <button class="b1 b2" onclick="rose1();">Pulsing</button>
+      <button class="b1 b2" onclick="vi(20000);">Steady</button>
+    </div>`
+    mkw(win, 'Rose Toy', '240px');
 }
 rosetoy();
