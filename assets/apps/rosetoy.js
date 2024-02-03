efbd26f95f92fa7b1f9a32a6661dcc0d61785c8f
randomuser691337 <154252167+randomuser691337@users.noreply.github.com>
2024-02-03 17:53:56 -0600

    fuck

--- assets/apps/rosetoy.js
+++ assets/apps/rosetoy.js
@@ -4,7 +4,7 @@ function rosetoy() {
     <p>Rose Toy app only supports devices with a vibration motor and Chrome on Android.</p>
     <p>Trying to relax yourself on an iPhone or anything that isnt an Android will not work and may crash your device.</p>
     <button class="b1" onclick="fesw('rose1', 'rose2');">Start</button></div>
-    <div id="rose2" class="hidden">
+    <div id="rose2" class="hide">
       <p>Rose Toy PLUS!</p>
       <p>Only works on Android w/ Chrome</p>
       <button class="b1 b2" onclick="rose1();">Pulsing</button>
