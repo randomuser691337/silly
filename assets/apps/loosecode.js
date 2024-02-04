1f3451870fcae1e290a361fbddfaec4080509682
randomuser691337 <154252167+randomuser691337@users.noreply.github.com>
2024-02-03 23:15:27 -0600

    make the death quicker

--- assets/apps/loosecode.js
+++ assets/apps/loosecode.js
@@ -23,6 +23,13 @@ async function noerrd() {
     await writevar('errd', 'n');
 }
 
+async function exsandcon() {
+    if (sandParam) {
+       burnitall();
+       sth('end');  
+    }
+}
+
 function exsand() {
     mkw(`<p>Are you sure you want to exit Sandbox?</p><p>All data in Sandbox will be erased, select 'Close' to cancel or 'Okay' to continue.</p><button class="b1" onclick="sth('end');">Okay</button>`, 'Sandbox', '350px');
 }
