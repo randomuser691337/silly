dc716f62b5e1f5627d4c779978564aa85035f071
randomuser691337 <154252167+randomuser691337@users.noreply.github.com>
2024-02-09 20:24:44 -0600

    Update ui.js

--- assets/code/ui.js
+++ assets/code/ui.js
@@ -120,10 +120,10 @@ function redir(url2) {
 function about(value) {
     const about = `<img style="border: none; width: 100px; height: 100px;" src="./assets/img/favicon.png"></img>
         <p>Version: <span class="ver">one sec</span></p>
-        <p>Last edited: <span class="lastedit">one sec</span></p>
-        <a onclick="embed('https://meower.xyz', 'About Embed', '520px', '340px');">Developer's Site</a> <a onclick="doc('./assets/other/creds.txt', 'WebDesk Credits', '420px', 'auto');">Creds</a>`;
+        <p>Last edit: <span class="lastedit">one sec</span></p>
+        <a onclick="embed('https://meower.xyz', 'About Embed', '520px', '340px');">Dev's Site</a> <a onclick="doc('./assets/other/creds.txt', 'WebDesk Credits', '420px', 'auto');">Creds</a>`;
     if (value === undefined) {
-        mkw(about, `WebDesk ${ver}`, '240px', './assets/img/favicon.png');
+        mkw(about, `About`, '190px', './assets/img/favicon.png');
     }
 
     masschange('ver', ver);
